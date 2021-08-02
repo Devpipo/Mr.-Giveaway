70ba2bdab2a280e83e9220b1c894ea5b4d797447
Devpipo <82604696+Devpipo@users.noreply.github.com>
2021-08-02 18:46:07 +0200

    Update help.js

--- source/commands/help.js
+++ source/commands/help.js
@@ -8,7 +8,7 @@ module.exports = class extends Command {
     }
 
     execute(message) {
-        //message.author.send("```mg!create <time-in-seconds> <giveaway-title>\nmg!delete <giveaway-id>```");
+        message.author.send("```mg!create <time-in-seconds> <giveaway-title>\nmg!delete <giveaway-id>```");
         message.channel.send("**Hi there!** I am known as Mr. Giveaway. While my origins are mostly unknown, people are known to associate me with Packer#9020. Ha. What fools. Tell me, are you looking for a automated giveaway? Well, are you? I have the perfect thing for you. Try `mg!invite` to get me into your server, then go through and start your own giveaway using the commands in `mg!commands`. I will be seeing you soon...");
     }
 };
